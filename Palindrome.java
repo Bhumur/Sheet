db563e87f8f0e3263e5bb74ba3af258edbb8e238
Bhumur Agrawal <57053398+Bhumur@users.noreply.github.com>
2025-04-06 11:32:36 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,12 @@
+package com.cdac.acts.functions.strings;
+
+public class Palindrome{
+	public static boolean isPalindrome(String s){
+		for(int i=0,j=s.length()-1;i<j;i++,j--){
+			if(s.charAt(i)!=s.charAt(j)){
+				return false;
+			}
+		}
+		return true;
+	}
+}
